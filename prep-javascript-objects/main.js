f6b377b51c77a682276d48228577720b57a460bf
Zoey1318 <gyu4485@gmail.com>
2022-08-19 06:40:41 +0000

    Completed prep-javascript-objects

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,13 @@
+const person = { firstName: 'Zoey', lastName: 'Kim', hobby: 'Playing games' };
+console.log('Person:', person);
+
+const fullName = 'Zoey' + ' ' + 'Kim';
+console.log('FullName:', fullName);
+
+person.job = 'Graphic' + ' ' + 'designer';
+console.log('Job:', person.job);
+
+person.previousJob = 'Apparel' + ' ' + 'production' + ' ' + 'manager';
+console.log('PreviousJob:', person.previousJob);
+
+console.log('Object Person:', person);
