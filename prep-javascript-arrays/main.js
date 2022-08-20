f82f08077e72a5878800907bec3c98a85ba7a09b
Zoey1318 <gyu4485@gmail.com>
2022-08-20 04:24:54 +0000

    Completed prep-javascript-arrays

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,14 @@
+const languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+console.log('Languages array:', languages);
+
+const firstElement = languages[0];
+console.log('FirstElement:', firstElement);
+
+const thirdElement = languages[2];
+console.log('ThirdElement:', thirdElement);
+
+const length = languages.length;
+console.log('Length of array:', length);
+
+const lastElement = languages[languages.length - 1];
+console.log('LastElement:', lastElement);
